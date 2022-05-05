4fbf921a4c4e97d26e07ee6972f0ad07abd0822d
luisgcodes <lcienfuegos95@gmail.com>
2022-05-04 18:55:03 -0700

    added dependencies

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-// setting const functions
+// Dependencies
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-// generates id's
+const data = require('./db/db.json');
 const {v4: uuid} = require('uuid');
-var notes = require('./db/db.json');
 
+// Sets up Express
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Formats data to json / access files from public / data parsing
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
