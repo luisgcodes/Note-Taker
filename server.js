c8f918092ac0928cf833bbb3041f2c5aeb269ba3
luisgcodes <lcienfuegos95@gmail.com>
2022-05-04 18:18:44 -0700

    created serverJS

---
+++ server.js
@@ -0,0 +1,8 @@
+// setting constants
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+// generates id's
+const {v4: uuid} = require('uuid');
+var notes = require('./db/db.json');
+
