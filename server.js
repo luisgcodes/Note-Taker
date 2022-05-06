0818ede62a4c591ed95b800427aec4011eae5e6e
luisgcodes <lcienfuegos95@gmail.com>
2022-05-05 20:15:20 -0700

    up and running

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const data = require('./db/db.json');
-const {v4: uuid} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 // Sets up Express
 const app = express();
@@ -13,6 +13,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/api/notes', (req,res) => {
+    res.json(data);
+});
 
 // Routes /notes to notes.html
 app.get('/notes', (req, res) => {
@@ -24,3 +28,47 @@ app.get('/index', (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+// Post sends the data to the body  
+app.post('/api/notes', (req,res) => {
+    const note = req.body;
+    console.log(note);
+
+    const addNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4()
+    }
+    data.push(addNote);
+    console.log(data);
+
+    updateData();
+
+    return res.json(addNote);
+});
+
+// Delets ID
+app.delete('/api/notes/:id', (req,res) => {
+    const requestId = parseInt(req.params.id);
+
+    data.forEach((note, i) => {
+        if (note.id === requestId) {
+            data.splice(i,1);
+            return res.json(note);
+        }
+    })
+    updateData();
+});
+
+function updateData() {
+    fs.writeFileSync("./db/db.json", JSON.stringify(data), err => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log("Success!")
+        };
+      });
+}
+// Listener
+app.listen(PORT, () => {
+    console.log(`Listening to http://localhost:${PORT}`)
+});
